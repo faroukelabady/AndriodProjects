367cb787cbeaf144ee7a70b6f81f7e78fa607cba
FaroukElabady <faroukelabady@hotmail.com>
2014-01-14 01:04:45 +0200

    Signed-off-by: FaroukElabady <faroukelabady@hotmail.com>

---
+++ com.solutions.andriod.tcpIpApp/src/com/solutions/andriod/tcpipapp/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.solutions.andriod.tcpipapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+	}
+
+	public void onClick(View view) {
+
+		EditText email = (EditText) findViewById(R.id.email);
+		EditText password = (EditText) findViewById(R.id.password);
+
+		if (!email.getText().toString().equals("7elol")
+				|| !password.getText().toString().equals("1234")) {
+
+			Toast.makeText(this, "Please enter a valid username and password",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+
+		System.out.print("here before changind activity");
+		Intent i = new Intent(this, MainActivity.class);
+		startActivity(i);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+}
